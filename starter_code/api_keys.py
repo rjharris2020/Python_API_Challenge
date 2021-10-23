67ed9b7fcb0d35009f7edd343716f9125f249ca1
Rashaye Harris <72972718+rjharris2020@users.noreply.github.com>
2021-10-23 18:05:59 -0400

    Updated WeatherPy.ipynb and VacationPy.ipynb

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "e98a8cb32fa1f3028b19b6c31f095459"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyDiuodRi6aVLcf3U7YIpKmj2XNfM80449U"
